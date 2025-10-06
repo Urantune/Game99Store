734a6d680b6c0e9606629626fa9b3af881aa2e4d
urantune <khangphucmn@outlook.com>
2025-10-06 16:39:40 +0700

    BEgin

--- src/main/java/WebBackEnd/App.java
+++ src/main/java/WebBackEnd/App.java
@@ -1,15 +1,18 @@
-package WebBackEnd.Controller;
+package WebBackEnd;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@EntityScan(basePackages = "entities")
+@SpringBootApplication
 public class App {
 
 
 
     public static void main(String[] args) {
 
-
-       // SpringApplication.run(App.class, args);
+        SpringApplication.run(App.class, args);
 
 
     }
--- src/main/java/WebBackEnd/Controller/HomeController.java
+++ src/main/java/WebBackEnd/Controller/HomeController.java
@@ -1,4 +1,36 @@
 package WebBackEnd.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value = "/welcome")
 public class HomeController {
+
+
+    @GetMapping("/home")
+    public String homepage(Model model) {
+     //   model.addAttribute("title", "Hehehe");
+        return "HTML/Index";
+    }
+
+
+//    @PostMapping("/home")
+//    public String doLogin(@RequestParam("username") String username,
+//                          @RequestParam("password") String password,
+//                          Model model) {
+//        if ("admin".equals(username) && "123".equals(password)) {
+//            model.addAttribute("title", "Xin chào " + username) ;
+//            return "home/welcome";
+//        } else {
+//            model.addAttribute("title", "Sai username hoặc password");
+//            return "home/welcome";
+//        }
+//    }
+
+
+
+
+
 }
