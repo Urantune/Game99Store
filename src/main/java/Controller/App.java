9cf623913084be17ddb43fb8ac62d08c02c104dd
urantune <khangphucmn@outlook.com>
2025-10-06 16:24:42 +0700

    Begin

---
+++ src/main/java/Controller/App.java
@@ -0,0 +1,25 @@
+package Controller;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+
+//@EntityScan(basePackages = "entities")
+public class App {
+
+
+
+    public static void main(String[] args) {
+
+
+       // SpringApplication.run(App.class, args);
+
+
+    }
+
+
+}
