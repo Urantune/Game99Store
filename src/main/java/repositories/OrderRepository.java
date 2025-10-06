b62b01dbdca804a8fc31744d3d06ed6ae6176b10
urantune <khangphucmn@outlook.com>
2025-10-06 16:02:49 +0700

    fix class

--- src/main/java/repositories/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package repositories;
-
-import model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-    // có thể viết thêm query nếu cần
-}
