27089586714d9d796251bbc003f9c3b113c9ae47
urantune <khangphucmn@outlook.com>
2025-10-06 16:34:34 +0700

    Begin

---
+++ src/main/java/WebBackEnd/App.java
@@ -0,0 +1,18 @@
+package WebBackEnd.Controller;
+
+
+//@EntityScan(basePackages = "entities")
+public class App {
+
+
+
+    public static void main(String[] args) {
+
+
+       // SpringApplication.run(App.class, args);
+
+
+    }
+
+
+}
---
+++ src/main/java/WebBackEnd/Controller/HomeController.java
@@ -0,0 +1,4 @@
+package WebBackEnd.Controller;
+
+public class HomeController {
+}
