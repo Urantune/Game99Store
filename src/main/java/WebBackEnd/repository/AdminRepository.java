df3b6834318fb6368ab3520621d3a7cda59d5c2e
urantune <khangphucmn@outlook.com>
2025-10-25 21:06:13 +0700

    Repo

--- src/main/java/WebBackEnd/repository/AdminRepository.java
+++ src/main/java/WebBackEnd/repository/AdminRepository.java
@@ -2,9 +2,10 @@ package WebBackEnd.repository;
 
 import WebBackEnd.Entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface AdminRepository extends JpaRepository<Admin, UUID> {
 
 
